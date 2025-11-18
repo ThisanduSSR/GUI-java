f901ee5c7b02bbbc9bb7fef6bbaff922514a016f
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 09:33:19 +0530

    GUI project

--- src/SimpleGUI.java
+++ src/SimpleGUI.java
@@ -1,5 +1,12 @@
 import javax.swing.*; //swing is a library that allows java to create GUIs like windows, buttons, labels, text fields, check boxes
 import javax.awt.*; //Abstract window Toolkit , swing needs AWT for layouts, colors, fonts, graphics
 public class SimpleGUI {
+    public static void main(String[] args){
 
+        JFrame frame = new JFrame("My first GUI"); //create a new window
+        frame.setSize(400,200);
+        frame.setLayout(new FlowLayout()); //tells the window how to arrange components
+
+        frame.add(new JLabel("Enter your name:"));
+    }
 }
