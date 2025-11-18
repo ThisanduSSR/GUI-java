9de9f1f8c680c0e41463339f77a266e05e7157ea
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 09:44:57 +0530

    GUI project

--- src/SimpleGUI.java
+++ src/SimpleGUI.java
@@ -8,5 +8,13 @@ public class SimpleGUI {
         frame.setLayout(new FlowLayout()); //tells the window how to arrange components
 
         frame.add(new JLabel("Enter your name:"));
+        frame.add(new JTextField("Type here", 15)); //width take spase after here eg.- type here - _______
+        frame.add(new JButton("OK"));
+        frame.add(new JCheckBox("Bold"));
+        frame.add(new JRadioButton("Red"));
+        frame.add(new JComboBox<>(new String[]{"Red","Green","Blue"})); //Add a dropdown list with 3 choices
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //This makes sure that when u click the  X button on the window, the entire program end. if u didnt add this the window closes but the program still run in the background
+        frame.setVisible(true); //this line display the window, if u dont wirte this line, the window will not appear.
+
     }
 }
