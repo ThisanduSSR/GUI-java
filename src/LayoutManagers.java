b1b1f26555f78ee1e45066032e53b4a95c3d42a2
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 10:23:23 +0530

    GUI project

--- src/LayoutManagers.java
+++ src/LayoutManagers.java
@@ -1,2 +1,22 @@
-public class LayoutManagers {
-}
+// A layout decides how components are arranged like
+// 1. FlowLayout - items are placed left to right
+// 2. GridLayout - items arranged like a chess board
+// 3. BorderLayout - window just like a map
+
+//colors
+//u can change button colors
+button.setBackground(Color.YELLOW);
+button.setForeground(Color. RED);
+
+//foreground color - text color
+//Background color - button color
+
+//fonts
+Font f = new Font("Serif", Font.BOLD, 18);
+button.setFont(f);
+
+//JTable
+//JTable is like an excel sheet inside ur java program
+// Rows - horizontal
+//Columns - vertical
+//cells - boxes of data
