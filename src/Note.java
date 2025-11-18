3a6ad2b05e726343739cd00440f69ee230500273
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 09:19:56 +0530

    GUI project

---
+++ src/Note.java
@@ -0,0 +1,21 @@
+//components = things u put inside the window
+// its like imaging ur lunchbox, inside the lunch box we can put label , button, text field, checkbox, radio button, combo box
+
+//label-just shows text  e.g.-Enter name
+JLabel lbl=new JLabel("Enter your name:");
+
+//text field - user types inside
+JTextField txt = new JTextField("Type here");
+
+// button - like OK or submit
+Jbutton btn = new JButton("OK");
+
+//checkbox - like a tick box
+JCheckBox chk = new JCheckBox("Bold");
+
+//radio button - circle selection
+JRadioButton r = new JRadioButton("Red");
+
+//combo box - dropdown list
+JComboBox colorBox = new JComboBox(new String[] {"red","green","blue"});
+
