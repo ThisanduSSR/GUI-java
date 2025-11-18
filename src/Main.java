9cbf5765576e0a35da4bf5f8939f7714add63635
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 12:04:41 +0530

    GUI project

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,25 @@ public class Main {
         ArrayList<Book> bookList = new ArrayList<>();
 
         while(true) {
+            System.out.println("\nPress 1 to add a book");
+            System.out.println("press 0 to exit");
+            int choice =  s.nextInt();
+            s.nextLine();
+
+            if(choice==1){
+                System.out.print("Enter title:");
+                String title  = s.nextLine();
+
+                System.out.print("Enter year:");
+                String author = s.nextLine();
+
+                System.out.print("Enter price");
+                int year = s.nextInt();
+
+                Book b = new Book(title, year, author);
+                b.setPrice(rpice);
+
+            }
 
         }
     }
