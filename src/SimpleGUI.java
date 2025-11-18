efa0d3715f6a8c26e500f5bc6018f5b4bc4acc12
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-18 09:28:10 +0530

    GUI project

---
+++ src/SimpleGUI.java
@@ -0,0 +1,5 @@
+import javax.swing.*; //swing is a library that allows java to create GUIs like windows, buttons, labels, text fields, check boxes
+import javax.awt.*; //Abstract window Toolkit , swing needs AWT for layouts, colors, fonts, graphics
+public class SimpleGUI {
+
+}
