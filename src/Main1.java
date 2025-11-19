5da8ef980da01000baf2a0212c92a8fc84de3a09
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-19 09:56:49 +0530

    GUI project

--- src/Main1.java
+++ src/Main1.java
@@ -10,5 +10,19 @@ public class Main1 {
             this.author = author;
             this.year = year;
         }
+        public void printDetails() {
+            System.out.println("Title:"+title);
+            System.out.println("Year:"+year);
+            System.out.println("Author:"+author);
+        }
+        public void updateYear(int newYear){
+            this.year = newYear;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public int getBookAge(int currentYear){
+            return currentYear - year;
+        }
     }
 }
