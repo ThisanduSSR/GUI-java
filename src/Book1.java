851a9ba5c05e125af05192d438842d3af2b9989c
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-19 09:18:39 +0530

    GUI project

--- src/Book1.java
+++ src/Book1.java
@@ -4,5 +4,10 @@ public class Book1 {
 
     public void printDetails() {
         System.out.println(title+" "+author);
+
+        Book1 b1 = new Book1();
+        b1.title = "Harry poter";
+        b1.author = "Rowling";
+        b1.printDetails();
     }
 }
