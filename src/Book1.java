216208ecb79ab103503be842e5f703e56aa5eafd
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-11-19 09:51:46 +0530

    GUI project

--- src/Book1.java
+++ src/Book1.java
@@ -6,7 +6,7 @@ public class Book1 {
         System.out.println(title+" "+author);
 
         Book1 b1 = new Book1();
-        b1.title = "Harry poter";
+        b1.title = "Harry potter";
         b1.author = "Rowling";
         b1.printDetails();
     }
---
+++ src/Main1.java
@@ -0,0 +1,14 @@
+public class Main1 {
+    //Book class(inner class)
+    public static class Book{
+        private String title;
+        private String author;
+        private int year;
+
+        public Book(String title, String author, int year) {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+    }
+}
